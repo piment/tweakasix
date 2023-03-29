62a26e0d224a4bf261b2dbf81f9899d00d424627
Barren <xaboule@gmail.com>
2023-03-29 15:45:08 +0200

    selected options summed

--- client/src/components/Selector.jsx
+++ client/src/components/Selector.jsx
@@ -2,57 +2,75 @@ import { useState, useEffect } from "react";
 import React from "react";
 import axios from "axios";
 
-
 function Selector() {
-    const [itemsList, setItemsList] = useState([]);
-    const body = []
-    const neck = []
+  const [itemsList, setItemsList] = useState([]);
+  const [boPrice, setBoPrice] = useState(0);
+  const [nePrice, setNePrice] = useState(0);
+  const [piPrice, setPiPrice] = useState(0);
 
 
-    const getItems = () => {
-        axios.get("http://localhost:3001/items", {}).then((res) => {
-            // console.log(res.data)
-          setItemsList(res.data);
-        });
-        // for(let i = 0 ; i< itemsList.length; i++){
-        //   if(itemsList[i].id_category == 1){
-        //     body.push(itemsList[i])
-        //   } else if (itemsList[i].id_category == 2){
-        //     neck.push(itemsList[i])
-        //   }
-        // }
-      };
-      console.log(itemsList)
-      
-      
-      useEffect(() => {
-        getItems()
-        console.log('body:' + body.length + 'neck:' + neck.length )
-    }, [])
+
+  const getItems = () => {
+    axios.get("http://localhost:3001/items", {}).then((res) => {
+      setItemsList(res.data);
+    });
+  };
+
+
+
+console.log(boPrice)
+
+
+let total = (parseInt(boPrice)+parseInt(nePrice)+parseInt(piPrice))
+
+
+
+
+
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+
   return (
+    <div>
     <div className="selector-section">
-    Body:
-      <select name="" id="">
-     {itemsList.filter(item => item.id_category === 1).map(filteredItem => (
-    <option>
-      {filteredItem.name} {filteredItem.price}$
-    </option>
-  ))}
-      </select> 
+      Body:
+      <select name="" id="" onChange={(e) => {setBoPrice(e.target.value)}}>
+        {itemsList
+          .filter((item) => item.id_category === 1)
+          .map((filteredItem) => (
+            <option value={filteredItem.price}>
+              {filteredItem.name} {filteredItem.price}$
+            </option>
+          ))}
+      </select>
       Neck:
-      <select name="" id="">
-      {itemsList.filter(item => item.id_category === 2).map(filteredItem => (
-    <option>
-      {filteredItem.name} {filteredItem.price}$
-    </option>
-  ))}
+      <select name="" id="" onChange={(e) => setNePrice(e.target.value)}>
+        {itemsList
+          .filter((item) => item.id_category === 2)
+          .map((filteredItem) => (
+            <option value={filteredItem.price}>
+              {filteredItem.name} {filteredItem.price}$
+            </option>
+          ))}
       </select>
       Pickups
-      <select name="" id=""></select>
+      <select name="" id="" onChange={(e) => setPiPrice(e.target.value)}>
+        {itemsList
+          .filter((item) => item.id_category === 3)
+          .map((filteredItem) => (
+            <option value={filteredItem.price}>
+              {filteredItem.name} {filteredItem.price}$
+            </option>
+          ))}
+      </select>
       Metal:
       <select name="" id=""></select>
-      Wiring :
-      <select name="" id=""></select>
+      Wiring :<select name="" id=""></select>
+    </div>
+    <h3>{total}</h3>
     </div>
   );
 }
