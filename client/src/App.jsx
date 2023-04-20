9b761aada868a77ba6acb599afbe3a506f086e40
Barren <xaboule@gmail.com>
2023-04-20 02:25:12 +0200

    concept added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Home from "./components/Home";
 import Parts from "./components/Parts";
 import Account from "./components/Account";
 import Cart from "./components/Cart";
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <div className="App">
-
+              <Navbar/>
     <Router>
             <Routes>
               <Route
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
   return (
     <div className="navbar-main">
       <div >
-        <ul className="navbar-cont">
             <div id="triangle-code"></div>
+        <ul className="navbar-cont">
           <li className="indiv-parts"  ><a href="/parts" >Spare parts</a></li>
           <li className="account"> <a href="/account"> My Account</a></li>
           <li className="cart"><a href="/cart">My Cart</a></li>
