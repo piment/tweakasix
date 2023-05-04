e8a438fbd0a25cea8e926c97c3fe4e139e62f997
Barren <xaboule@gmail.com>
2023-05-04 10:41:22 +0200

    slash routes removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,14 +16,14 @@ function App() {
       <Navbar/>
             <Routes>
               <Route
-                path=""
+                path="/"
                 element={<Home/>}
               />
               <Route
-                path="parts"
+                path="/parts"
                 element={<Parts />}
               />
-              <Route path="account" element={<Account/>} />
+              <Route path="/account" element={<Account/>} />
               <Route path="cart" element={<Cart />} />
             </Routes>
           </Router>
