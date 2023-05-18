c176ffca91f4223d77afa3ba4a8334109911c7b3
Barren <xaboule@gmail.com>
2023-05-18 19:14:06 +0200

    userinfo persist and display

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -1,62 +1,72 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import Registration from './Register'
-import './account.css'
+import Registration from "./Register";
+import "./account.css";
 
 function Account() {
   const [loginStatus, setLoginStatus] = useState(false);
-  const [userInfo, setUserInfo] = useState()
+  const [userInfo, setUserInfo] = useState();
 
-// const getUserInfo = () => {
-//   Axios.get('http://localhost:3001/getUserInfo',{
-//     username : userInfo.username
-//   }).then((response) => console.log(response))
-// }
-const logout = () => {
-  // console.log('pioupiou')
-  // console.log(localStorage.length)
-  localStorage.removeItem("token");
-  setLoginStatus(false)
-};
-const storedUserString = localStorage.getItem('userInfo')
-const storeUser = JSON.parse(storedUserString)
+  const toPascalCase = str => (str.match(/[a-zA-Z0-9]+/g) || []).map(w => `${w.charAt(0).toUpperCase()}${w.slice(1)}`).join(' ');
 
-console.log(userInfo)
-useEffect(() => {
 
-setUserInfo(storeUser)
-  // console.log(localStorage)
-},[setUserInfo])
+  const logout = () => {
+    localStorage.removeItem("token");
+    setLoginStatus(false);
+  };
+  const storedUserString = localStorage.getItem("userInfo");
+  const storeUser = JSON.parse(storedUserString);
 
+
+  useEffect(() => {
+    setUserInfo(storeUser);
+
+  }, [setUserInfo]);
 
   return (
     <div className="account-main">
-      {/* <Construction/> */}
-      {/* {loginStatus !== true &&( */}
-     {!localStorage.getItem("token")&& (
-            <>
-      <Registration loginStatus={loginStatus} setLoginStatus={setLoginStatus} userInfo={userInfo} setUserInfo={setUserInfo}/>
-      </>
-     )}
-       {localStorage.getItem("token")&& (
+
+      {!localStorage.getItem("token") && (
+        <>
+          <Registration
+            loginStatus={loginStatus}
+            setLoginStatus={setLoginStatus}
+            userInfo={userInfo}
+            setUserInfo={setUserInfo}
+          />
+        </>
+      )}
+      {localStorage.getItem("token") && userInfo && (
         <div className="user-auth-true">
-
-<div className="user-welcome">
- <h1> Welcome {userInfo.username}</h1>
-</div>
-
-<div className="user-info"></div>
-
-<div className="logout-wrap">
-
-        <button className="logout" onClick={logout}>Log out</button>
-</div>
-        
-        
+          <div className="user-welcome">
+            <h1> Welcome <span id="username">{toPascalCase(userInfo.username)}</span></h1>
+          </div>
+        <div className="dashboard">
+          <div className="user-infos">
+            <div className="personnal-infos">
+              <ul id="list">
+                <li>{toPascalCase(userInfo.username)}</li>
+                <li>
+                  {userInfo.firstname} {userInfo.lastname}
+                </li>
+                <li>{userInfo.email}</li>
+                <li>adress</li>
+              </ul>
+            </div>
+          </div>
+<div className="user-activities">
+          <div className="order-history"> No orders yet</div>
+          <div className="saved-guitars"> Start tweaking your six strings now!</div></div>
+          </div>
+          <div className="logout-wrap">
+            <button className="logout" onClick={logout}>
+              Log out
+            </button>
+          </div>
         </div>
-       )}
+      )}
     </div>
-  )
+  );
 }
 
-export default Account
+export default Account;
